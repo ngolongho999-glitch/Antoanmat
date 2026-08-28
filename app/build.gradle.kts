21b4ae959ef7c92b4c259821ce4ab0b764ec3880
ngolongho999-glitch <ngolongho999@gmail.com>
2026-08-28 11:06:16 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,42 +5,37 @@ plugins {
 
 android {
     namespace = "vn.ngolongho.antoanmat"
-    compileSdk = 35
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "vn.ngolongho.antoanmat"
-        minSdk = 29
-        targetSdk = 35
-
+        minSdk = 26
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
     }
 
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-
     kotlinOptions {
         jvmTarget = "17"
     }
 }
 
 dependencies {
-
-    implementation("androidx.core:core-ktx:1.15.0")
-
-    implementation("androidx.appcompat:appcompat:1.7.0")
-
-    implementation("androidx.activity:activity-ktx:1.10.0")
-
-    implementation("androidx.lifecycle:lifecycle-service:2.8.7")
-
-    implementation("androidx.camera:camera-core:1.4.1")
-
-    implementation("androidx.camera:camera-camera2:1.4.1")
-
-    implementation("androidx.camera:camera-lifecycle:1.4.1")
-
-    implementation("com.google.mlkit:face-detection:16.1.7")
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 }
