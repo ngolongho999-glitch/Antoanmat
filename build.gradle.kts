5796e55777b27a4f379a2aedf4ca263bf20bc765
ngolongho999-glitch <ngolongho999@gmail.com>
2026-09-02 19:33:46 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,13 @@
-plugins {
-    id("com.android.application") version "8.2.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+dependencies {
+    // CameraX
+    val cameraVersion = "1.3.4"
+    implementation("androidx.camera:camera-camera2:$cameraVersion")
+    implementation("androidx.camera:camera-lifecycle:$cameraVersion")
+    implementation("androidx.camera:camera-view:$cameraVersion")
+
+    // Google ML Kit Face Detection
+    implementation("com.google.mlkit:face-detection:16.1.6")
+
+    // Lifecycle Service cho CameraX trong Background
+    implementation("androidx.lifecycle:lifecycle-service:2.8.4")
 }
